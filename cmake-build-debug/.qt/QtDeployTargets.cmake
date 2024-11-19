1f213bc39142941e56a114dd66cfa09b4dfbb758
Aleksey Kutanov <aleksey.kutanov@x5.ru>
2024-11-19 21:47:24 +0300

    clean repo

--- cmake-build-debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_course_project_FILE /Applications/Development/C_BSUIR/C++_5/ArchiveIT/cmake-build-debug/course_project)
-set(__QT_DEPLOY_TARGET_course_project_TYPE EXECUTABLE)
