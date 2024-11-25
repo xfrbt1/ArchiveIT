43eced486caf6c04aa124dc6c83ea81bc4d26a00
Aleksey Kutanov <aleksey.kutanov@x5.ru>
2024-11-25 09:05:49 +0300

    file manager adapter

--- src/Adapter.cpp
+++ src/Adapter.cpp
@@ -1,3 +1,15 @@
-//
-// Created by Алексей Алексеев on 30.10.24.
-//
+#include "../include/Adapter.h"
+
+
+void Adapter::updatePath(std::string &path)
+{
+    fileManager->setPath(path);
+    fileManager->setInfo();
+    fileManager->getInfo();
+}
+
+
+void Adapter::remove()
+{
+    fileManager->remove();
+}
---
+++ src/FileManager.cpp
@@ -0,0 +1,130 @@
+#include "../include/FileManager.h"
+
+
+int FileManager::setPath(std::string &newPath)
+{
+    if (std::filesystem::exists(newPath))
+    {
+        currentPath = newPath;
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+
+int FileManager::setPathObjectType()
+{
+    if (std::filesystem::is_regular_file(currentPath))
+    {
+        isRegular = true;
+        return 1;
+    }
+    if (std::filesystem::is_directory(currentPath))
+    {
+        isRegular = false;
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+
+int FileManager::setSize()
+{
+    if (isRegular)
+    {
+        size = std::filesystem::file_size(currentPath);
+        return 1;
+    }
+    if (!isRegular)
+    {
+        uintmax_t totalSize = 0;
+        if (std::filesystem::exists(currentPath) && std::filesystem::is_directory(currentPath))
+        {
+            for (const auto& entry : std::filesystem::recursive_directory_iterator(currentPath))
+            {
+                if (std::filesystem::is_regular_file(entry.path()))
+                {
+                    totalSize += std::filesystem::file_size(entry.path());
+                }
+            }
+        }
+        size = totalSize;
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+
+int FileManager::setLastModified()
+{
+    auto f_updated_time = std::filesystem::last_write_time(currentPath);
+    auto sys_updated_time = std::chrono::time_point_cast<std::chrono::system_clock::duration>
+            (
+                    f_updated_time - std::filesystem::file_time_type::clock::now() + std::chrono::system_clock::now()
+            );
+    lastModified = time_to_string(sys_updated_time);
+    return 1;
+}
+
+
+void FileManager::setInfo()
+{
+    if (currentPath.empty())
+    {
+        return;
+    }
+    setPathObjectType();
+    setSize();
+    setLastModified();
+}
+
+
+void FileManager::getInfo()
+{
+    std::cout << "path: " << currentPath << std::endl;
+    std::cout << "type: " << (isRegular ? "regular" : "directory") << std::endl;
+    std::cout << "size: " << size << std::endl;
+    std::cout << "lastModified: " << lastModified << std::endl;
+}
+
+
+void FileManager::remove()
+{
+    if (currentPath.empty())
+    {
+        return;
+    }
+    try
+    {
+        auto path = std::filesystem::path(currentPath);
+
+        if (std::filesystem::exists(path))
+        {
+            if (std::filesystem::is_directory(path))
+            {
+                std::filesystem::remove_all(path);
+            }
+            else
+            {
+                std::filesystem::remove(path);
+            }
+        }
+        else
+        {
+            return;
+        }
+    }
+    catch (const std::filesystem::filesystem_error &e)
+    {
+        std::cout << "remove error: " << e.what() << std::endl;
+    }
+}
